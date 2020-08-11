3985f22ac95ccebacb3f0a8c658c56393bebce5b
stark1996 <stevenfernz96@gmail.com>
2020-08-11 17:51:16 +0530

    First Commit!

---
+++ app/src/main/java/com/example/attendancesystem/scannerCam.java
@@ -0,0 +1,14 @@
+package com.example.attendancesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scannerCam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_cam);
+    }
+}
